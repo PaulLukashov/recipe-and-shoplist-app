fdd58c8d74d91a1fcd98b8e8eda8768bee1b739d
Pavel <p.lukashov@section228.com>
2021-10-08 15:59:10 +0300

    add lazy loaded modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   {
@@ -7,10 +7,28 @@ const appRoutes: Routes = [
     redirectTo: '/recipes',
     pathMatch: 'full',
   },
+  {
+    path: 'recipes',
+    loadChildren: () =>
+      import('./recipes/recipes.module').then((m) => m.RecipeModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'shopping-list',
+    loadChildren: () =>
+      import('./shopping-list/shopping-list.module').then(
+        (m) => m.ShoppingListModule
+      ),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core.module';
 import { HeaderComponent } from './header/header.component';
-import { RecipeModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -18,11 +15,8 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    RecipeModule,
-    ShoppingListModule,
     SharedModule,
     CoreModule,
-    AuthModule,
   ],
   bootstrap: [AppComponent],
 })
